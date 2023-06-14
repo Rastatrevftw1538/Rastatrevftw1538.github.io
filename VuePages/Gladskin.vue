a72e547e6a55569a70eb89b05f17233c98237063
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2023-06-14 16:01:21 -0700

    Added javascript to the pages with multiple images

---
+++ VuePages/Gladskin.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h2>GladskinMD Battle of the Biome</h2>
+    <img src="../images/Gladskin-Logo.png" alt="Gladskin Logo" width="200">
+    <p>A demonstrative demo of how GladskinMD work for inflammatory skin conditions.</p>
+</div>
+</template>
+
+<script>
+export default {
+  // component logic goes here
+}
+</script>
+
+<style scoped>
+/* component styles go here */
+</style>
